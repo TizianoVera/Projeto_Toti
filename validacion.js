8e77e7a415481e47a97f90b1f08b362ded936e9d
TizianoVera <tiziano.vera90@gmail.com>
2021-09-04 02:53:18 -0300

    un pobre actualizacion

--- validacion.js
+++ validacion.js
@@ -1,16 +1,53 @@
-const form = document.getElementById('form');
-const nome = document.getElementById('nome');
-const telefone = document.getElementById('telefone');
-const email = document.getElementById('email');
-const asunto = document.getElementById('asunto')
-const mensaje = document.getElementById('mensaje')
+let formulario = document.getElementById('form');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', e =>{
     e.preventDefault();
-    checkInputs();
+
+    let formData = new formData(form);
+        
+               const contato = {
+            nome: formData.get('nome').value.trim(),
+            telefone: formData.get('telefone').value.trim(),
+            email: formData.get('email').value.trim(),
+            mensaje: formData.get('mensaje').value.trim()
+    };
+     
+    formData.push(contato);
 });
 
-function checkInputs(){
-    const nomeValue = nome.value.trim()
-     
+let erros = [];
+
+if (!validarCampo(contato.nome)) {
+    erros.push("O nome é obrigatorio");
+} else { 
+    alert('ingreso obrigatorio')
+}
+
+if (!validarTelefone(contato.telefone)) {
+    erros.push("O telefone é obrigatorio")
+} else{
+    alert('ingreso obrigatorio')
+}
+
+if (!validarEmail(contato.email)) {
+    erros.push("O email é obrigatorio")
+} else{
+    alert('ingreso obrigatorio')
+}
+
+
+function validarCampo(valor){
+    return !!valor
+};
+
+function validarTelefone(valor) {
+    /^[0-9]{2}-[0-9]{5}-[0-9]{4}$/.test(valor)
+};
+
+function validarEmail(valor){
+    return /^\S+@\S+$/.test(valor)
+};
+
+function lenghtMesaje (valor){
+    return valor.length != 50;
 }
